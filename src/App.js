beac30231d4abb518867bea154e8190cf41903d8
Mohamed Numan <73037918+num4n1@users.noreply.github.com>
2024-03-24 19:35:17 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateJobPage from './CreateJobPage';
+import './App.css'; // Your App-level styles
+
+function App() {
+  return (
+    <div className="App">
+      <CreateJobPage />
+    </div>
+  );
+}
+
+export default App;
